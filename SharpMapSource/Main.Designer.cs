246a365aaf273d213ed507ee3ead8e43c383b617
Nikola Misic <nikola.misic@elfak.rs>
2013-01-23 15:28:03 +0100

    Dodate osnovne funkcionalnosti pregleda predefinisane mape, zoom i pan

---
+++ SharpMapSource/Main.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace SharpMapSource
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnZoomIn = new System.Windows.Forms.Button();
+            this.btnZoomOut = new System.Windows.Forms.Button();
+            this.btnZoomFull = new System.Windows.Forms.Button();
+            this.pbxMapa = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxMapa)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnZoomIn
+            // 
+            this.btnZoomIn.Location = new System.Drawing.Point(12, 12);
+            this.btnZoomIn.Name = "btnZoomIn";
+            this.btnZoomIn.Size = new System.Drawing.Size(75, 23);
+            this.btnZoomIn.TabIndex = 0;
+            this.btnZoomIn.Text = "Zoom in";
+            this.btnZoomIn.UseVisualStyleBackColor = true;
+            this.btnZoomIn.Click += new System.EventHandler(this.btnZoomIn_Click);
+            // 
+            // btnZoomOut
+            // 
+            this.btnZoomOut.Location = new System.Drawing.Point(93, 12);
+            this.btnZoomOut.Name = "btnZoomOut";
+            this.btnZoomOut.Size = new System.Drawing.Size(75, 23);
+            this.btnZoomOut.TabIndex = 1;
+            this.btnZoomOut.Text = "Zoom out";
+            this.btnZoomOut.UseVisualStyleBackColor = true;
+            this.btnZoomOut.Click += new System.EventHandler(this.btnZoomOut_Click);
+            // 
+            // btnZoomFull
+            // 
+            this.btnZoomFull.Location = new System.Drawing.Point(174, 12);
+            this.btnZoomFull.Name = "btnZoomFull";
+            this.btnZoomFull.Size = new System.Drawing.Size(75, 23);
+            this.btnZoomFull.TabIndex = 2;
+            this.btnZoomFull.Text = "Zoom to full";
+            this.btnZoomFull.UseVisualStyleBackColor = true;
+            this.btnZoomFull.Click += new System.EventHandler(this.btnZoomFull_Click);
+            // 
+            // pbxMapa
+            // 
+            this.pbxMapa.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pbxMapa.Location = new System.Drawing.Point(12, 41);
+            this.pbxMapa.Name = "pbxMapa";
+            this.pbxMapa.Size = new System.Drawing.Size(747, 301);
+            this.pbxMapa.TabIndex = 3;
+            this.pbxMapa.TabStop = false;
+            this.pbxMapa.Click += new System.EventHandler(this.pbxMapa_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(771, 354);
+            this.Controls.Add(this.pbxMapa);
+            this.Controls.Add(this.btnZoomFull);
+            this.Controls.Add(this.btnZoomOut);
+            this.Controls.Add(this.btnZoomIn);
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Main";
+            ((System.ComponentModel.ISupportInitialize)(this.pbxMapa)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnZoomIn;
+        private System.Windows.Forms.Button btnZoomOut;
+        private System.Windows.Forms.Button btnZoomFull;
+        private System.Windows.Forms.PictureBox pbxMapa;
+    }
+}
+
---
+++ SharpMapSource/Main.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SharpMapSource
+{
+    public partial class Main : Form
+    {
+        private SharpMap.Map _sharpMap;
+        private const float ZOOM_FACTOR = 0.3f;
+        const String DATA_NAME = "World Countries";
+        const String DATA_PATH = "Data//world_adm0.shp";
+
+        public Main()
+        {
+            InitializeComponent();
+            _sharpMap = new SharpMap.Map(this.pbxMapa.Size);
+            _sharpMap.BackColor = Color.WhiteSmoke;
+
+            SharpMap.Layers.VectorLayer layer = new SharpMap.Layers.VectorLayer(DATA_NAME);
+            layer.DataSource = new SharpMap.Data.Providers.ShapeFile(DATA_PATH);
+            layer.Style.Fill = Brushes.LightGreen;
+            layer.Style.EnableOutline = true;
+            layer.Style.Outline = Pens.DarkGreen;
+
+            SharpMap.Layers.LabelLayer labelLayer = new SharpMap.Layers.LabelLayer("Country Names");
+            labelLayer.DataSource = layer.DataSource;
+            labelLayer.LabelColumn = "NAME";
+            labelLayer.Style.CollisionDetection = true;
+            //labelLayer.Style.CollisionBuffer = new SizeF(10, 10);
+            labelLayer.LabelFilter = SharpMap.Rendering.LabelCollisionDetection.ThoroughCollisionDetection;
+            labelLayer.MultipartGeometryBehaviour = SharpMap.Layers.LabelLayer.MultipartGeometryBehaviourEnum.Largest;
+            labelLayer.Style.Font = new Font(FontFamily.GenericSansSerif, 8);
+
+            _sharpMap.Layers.Add(labelLayer);
+            _sharpMap.Layers.Add(layer);
+            _sharpMap.ZoomToExtents();
+
+            RefreshMap();
+        }
+
+        public void RefreshMap()
+        {
+            pbxMapa.Image = _sharpMap.GetMap();
+        }
+
+        private void btnZoomIn_Click(object sender, EventArgs e)
+        {
+            _sharpMap.Zoom -= _sharpMap.Zoom * ZOOM_FACTOR; 
+            RefreshMap();
+        }
+
+        private void btnZoomOut_Click(object sender, EventArgs e)
+        {
+            _sharpMap.Zoom += _sharpMap.Zoom * ZOOM_FACTOR;
+            RefreshMap();
+        }
+
+        private void btnZoomFull_Click(object sender, EventArgs e)
+        {
+            _sharpMap.ZoomToExtents();
+            RefreshMap();
+        }
+
+        private void pbxMapa_Click(object sender, EventArgs e)
+        {
+            if (e is MouseEventArgs)
+            {
+                MouseEventArgs m = e as MouseEventArgs;
+                //--> Convert mouse click point from image coordinates to world coordinates
+                SharpMap.Geometries.Point p = _sharpMap.ImageToWorld(new PointF(m.X, m.Y));
+                //--> Recenter map
+                _sharpMap.Center.X = p.X;
+                _sharpMap.Center.Y = p.Y;
+                RefreshMap();
+            }
+        }
+    }
+}
