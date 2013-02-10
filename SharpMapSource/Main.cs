bc7451994f88444e30ac2ae7a99a55828e008677
Nikola Misic <nikola.misic@elfak.rs>
2013-02-10 14:09:36 +0100

    dodati prostorni upiti

--- SharpMapSource/Main.cs
+++ SharpMapSource/Main.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+<<<<<<< HEAD
+=======
+using System.IO;
+>>>>>>> Inicijalna verzija
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -8,32 +12,86 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using OSGeo.GDAL;
+<<<<<<< HEAD
 using SharpMap;
+=======
+using SharpMapSource.Layers;
+>>>>>>> Inicijalna verzija
 
 namespace SharpMapSource
 {
     public partial class Main : Form
     {
         private SharpMap.Map _sharpMap;
+<<<<<<< HEAD
         private const float ZOOM_FACTOR = 0.3f;
         private String DATA_NAME = "World Countries";
         private String DATA_PATH = "Data//world_adm0.shp";
         SharpMap.Geometries.LinearRing select;
+=======
+        private LayerManager _manager;
+
+        private const float ZOOM_FACTOR = 0.3f;
+        //private String DATA_NAME = "World Countries";
+        //private String DATA_PATH = "Data//world_adm0.shp";
+        //pan image
+        private Point _panCoordinate;
+
+>>>>>>> Inicijalna verzija
 
         public Main()
         {
             InitializeComponent();
+<<<<<<< HEAD
             select = new SharpMap.Geometries.LinearRing();
             _sharpMap = new SharpMap.Map(this.pbxMapa.Size);
             _sharpMap.BackColor = Color.WhiteSmoke;
 
             /*String path;
+=======
+            _sharpMap = new SharpMap.Map(this._sharpMapImage.Size);
+            _sharpMap.BackColor = Color.White;
+            this._manager = new LayerManager(this._sharpMap);
+
+            RefreshMap();
+        }
+
+        public void RefreshMap()
+        {
+            if (_sharpMap.Layers.Count != 0)
+                this._sharpMapImage.Image = _sharpMap.GetMap();
+            else
+                this._sharpMapImage.Image = null;
+        }
+
+        private void btnRemoveLayer_Click(object sender, EventArgs e)
+        {
+            if (this._dataGridLayers.SelectedRows.Count > 0)
+            {
+                if (this._dataGridLayers.SelectedRows[0].Index >= 0)
+                {
+                    _sharpMap.Layers.RemoveAt(this._dataGridLayers.SelectedRows[0].Index);
+                    this._dataGridLayers.Rows.RemoveAt(this._dataGridLayers.SelectedRows[0].Index);
+                    if (_sharpMap.Layers.Count != 0)
+                    {
+                        _sharpMap.ZoomToExtents();
+                    }
+                    RefreshMap();
+                }
+            }
+        }
+
+        private void menyAddVectorLayer_Click(object sender, EventArgs e)
+        {
+            String path;
+>>>>>>> Inicijalna verzija
             OpenFileDialog dialog = new OpenFileDialog();
             dialog.Filter = "shp Files (.shp)|*.shp|All Files (*.*)|*.*";
             dialog.FilterIndex = 1;
             dialog.Multiselect = false;
             DialogResult res = dialog.ShowDialog();
             if (res == DialogResult.OK)
+<<<<<<< HEAD
                 path = dialog.FileName;
             else
                 path = DATA_PATH;
@@ -77,18 +135,88 @@ namespace SharpMapSource
         }
 
         private void btnZoomOut_Click(object sender, EventArgs e)
+=======
+            {
+                path = dialog.FileName;
+                if (File.Exists(path))
+                {
+                    string layerName = Path.GetFileNameWithoutExtension(path);
+                    this._manager.AddVectorLayer(layerName, new SharpMap.Data.Providers.ShapeFile(path));
+                    this._dataGridLayers.Rows.Add( true,layerName);
+                }
+            }
+            try
+            {
+                _sharpMap.ZoomToExtents();
+                RefreshMap();
+            }
+            catch (Exception)
+            {
+                //MessageBox.Show("Layer already inserted");
+            }
+        }
+
+        private void menuAddRasterLayer_Click(object sender, EventArgs e)
+        {
+            SharpMap.Layers.ILayer layer = _sharpMap.Layers["RasterLayer"];
+            String path;
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Multiselect = false;
+            DialogResult res = dialog.ShowDialog();
+            if (res == DialogResult.OK)
+            {
+                path = dialog.FileName;
+                if (File.Exists(path))
+                {
+                    this._manager.AddRasterLayer(path);
+                    this._dataGridLayers.Rows.Add( true, dialog.SafeFileName);
+                    RefreshMap();
+                }
+            }
+        }
+
+        private void menuAddLabelLayer_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void closeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Geografski informacioni sistemi i tehnologije");
+        }
+
+        private void toolZoomIn_Click(object sender, EventArgs e)
+        {
+            _sharpMap.Zoom -= _sharpMap.Zoom * ZOOM_FACTOR;
+            RefreshMap();
+        }
+
+        private void toolZoomOut_Click(object sender, EventArgs e)
+>>>>>>> Inicijalna verzija
         {
             _sharpMap.Zoom += _sharpMap.Zoom * ZOOM_FACTOR;
             RefreshMap();
         }
 
+<<<<<<< HEAD
         private void btnZoomFull_Click(object sender, EventArgs e)
         {
             if(_sharpMap.Layers.Count != 0)
+=======
+        private void toolZoomToExtends_Click(object sender, EventArgs e)
+        {
+            if (_sharpMap.Layers.Count != 0)
+>>>>>>> Inicijalna verzija
                 _sharpMap.ZoomToExtents();
             RefreshMap();
         }
 
+<<<<<<< HEAD
         private void pbxMapa_Click(object sender, EventArgs e)
         {
             if (e is MouseEventArgs)
@@ -171,11 +299,31 @@ namespace SharpMapSource
                     //--> Recenter map
                     _sharpMap.Center.X = p.X;
                     _sharpMap.Center.Y = p.Y;
+=======
+        private void toolEditLayer_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolDeleteLayer_Click(object sender, EventArgs e)
+        {
+            if (this._dataGridLayers.SelectedRows.Count > 0)
+            {
+                if (this._dataGridLayers.SelectedRows[0].Index >= 0)
+                {
+                    _sharpMap.Layers.RemoveAt(this._dataGridLayers.SelectedRows[0].Index);
+                    this._dataGridLayers.Rows.RemoveAt(this._dataGridLayers.SelectedRows[0].Index);
+                    if (_sharpMap.Layers.Count != 0)
+                    {
+                        _sharpMap.ZoomToExtents();
+                    }
+>>>>>>> Inicijalna verzija
                     RefreshMap();
                 }
             }
         }
 
+<<<<<<< HEAD
         private void pbxMapa_MouseMove(object sender, MouseEventArgs e)
         {
             SharpMap.Geometries.Point p = _sharpMap.ImageToWorld(new PointF(e.X, e.Y));
@@ -255,6 +403,37 @@ namespace SharpMapSource
             }
             else
                 path = DATA_PATH;
+=======
+        private void sharpMapImage_MouseMove(SharpMap.Geometries.Point WorldPos, MouseEventArgs ImagePos)
+        {
+            this.lblWorldCoordiantes.Text = "World Coordinates: " + WorldPos.X + ":" + WorldPos.Y;
+            this.lblImageCoordinates.Text = "Image Coordinates: " + ImagePos.X + ":" + ImagePos.Y;
+        }
+
+        private void Main_SizeChanged(object sender, EventArgs e)
+        {
+            this._sharpMap.Size = this._sharpMapImage.Size;
+            this.RefreshMap();
+        }
+
+        private void sharpMapImage_MouseUp(SharpMap.Geometries.Point WorldPos, MouseEventArgs ImagePos)
+        {
+            if (ImagePos.Button == System.Windows.Forms.MouseButtons.Left)
+            {
+                Point mapCenter = new Point(this._sharpMapImage.Size.Width / 2 - ImagePos.Location.X + this._panCoordinate.X
+                    , this._sharpMapImage.Size.Height / 2 - ImagePos.Location.Y + this._panCoordinate.Y);
+                this._sharpMap.Center = this._sharpMap.ImageToWorld(mapCenter);
+                this.RefreshMap();
+            }
+        }
+
+        private void _sharpMapImage_MouseDown(SharpMap.Geometries.Point WorldPos, MouseEventArgs ImagePos)
+        {
+            if(ImagePos.Button == System.Windows.Forms.MouseButtons.Left)
+            {
+                this._panCoordinate = ImagePos.Location;
+            }
+>>>>>>> Inicijalna verzija
         }
     }
 }
